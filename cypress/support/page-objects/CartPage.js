bbff337e0e323b35dcf57fe36389780b5ce10832
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-26 16:45:01 -0300

    updating page objects

--- cypress/support/page-objects/CartPage.js
+++ cypress/support/page-objects/CartPage.js
@@ -1,10 +1,26 @@
 class CartPage {
   locators = {
+    section: '#cart_items',
+    emptyCartState: '#empty_cart',
     proceedToCheckoutButton: '.check_out',
+    removeButton: '.cart_quantity_delete',
   };
 
   clickProceedToCheckoutButton() {
     cy.get(this.locators.proceedToCheckoutButton).click();
+  }
+
+  clearCartItems() {
+    cy.get(this.locators.emptyCartState)
+      .invoke('attr', 'style')
+      .then((style) => {
+        cy.log(style);
+        if (style === 'display: none;') {
+          cy.get(this.locators.removeButton).each(($el) => {
+            cy.wrap($el).click();
+          });
+        }
+      });
   }
 }
 
--- cypress/support/page-objects/CheckoutPage.js
+++ cypress/support/page-objects/CheckoutPage.js
@@ -1,7 +1,31 @@
 class CheckoutPage {
   locators = {
-    totalPrice: '.cart_total_price',
+    firstProductName: 'tbody tr:nth-of-type(1) h4',
+    firstProductPrice: 'tbody tr:nth-of-type(1) .cart_price p',
+    firstProductQuantity: 'tbody tr:nth-of-type(1) .cart_quantity button',
+    firstProductTotal: 'tbody tr:nth-of-type(1) .cart_total p',
+    orderTotalPrice: 'tbody tr:nth-of-type(2) .cart_total_price',
   };
+
+  validateFirstProductName(name) {
+    cy.get(this.locators.firstProductName).should('have.text', name);
+  }
+
+  validateFirstProductPrice(price) {
+    cy.get(this.locators.firstProductPrice).should('have.text', price);
+  }
+
+  validateFirstProductQuantity(quantity) {
+    cy.get(this.locators.firstProductQuantity).should('have.text', quantity);
+  }
+
+  validateFirstProductTotal(productTotal) {
+    cy.get(this.locators.firstProductTotal).should('have.text', productTotal);
+  }
+
+  validateOrderTotal(orderTotal) {
+    cy.get(this.locators.orderTotalPrice).should('have.text', orderTotal);
+  }
 }
 
 export const checkoutPage = new CheckoutPage();
